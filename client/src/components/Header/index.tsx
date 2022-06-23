43a4832ff10f26d4f46de58b31d0581dcd35d344
AmitrajitDas <amitrajitdas31@gmail.com>
2022-06-23 13:51:23 +0530

    feat: navbar

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { IonHeader, IonTitle, IonToolbar } from "@ionic/react"
+
+const Header: React.FC = () => (
+  <IonHeader>
+    <IonToolbar color='tertiary'>
+      <IonTitle>Currency Converter</IonTitle>
+    </IonToolbar>
+  </IonHeader>
+)
+
+export default Header
