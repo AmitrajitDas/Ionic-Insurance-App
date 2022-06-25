e56e742bfd4c3c0b29f39827485c22ca69496677
AmitrajitDas <amitrajitdas31@gmail.com>
2022-06-25 12:33:24 +0530

    fix: session maxAge

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const colors = require("colors")
 const morgan = require("morgan")
 const db = require("./config/db")
 const bodyParser = require("body-parser")
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const userRouter = require('./routes/user')
-const passport = require('passport')
-const passport_local = require('./config/passport-local-auth')
-const MongoStore = require('connect-mongo')
-const passportJWT = require('./config/passport-jwt-strategy')
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const userRouter = require("./routes/user")
+const passport = require("passport")
+const passport_local = require("./config/passport-local-auth")
+const MongoStore = require("connect-mongo")
+const passportJWT = require("./config/passport-jwt-strategy")
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
 
 dotenv.config()
@@ -21,37 +21,40 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
-app.use(cors({
-  origin:'*'
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+)
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
 
 app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-
-app.use(session({
-  name: 'InsuranceApp',
-  secret: 'blahblahblah...',
-  saveUninitialized:false,
-  resave:false,
-  cookie:{
-      maxAge: (1000*60*100)
-  },
-  store: MongoStore.create({
-      mongoUrl: `mongodb://127.0.0.1:27017/${process.env.CONNECTION_URL}`,
-      autoRemove:'disabled',
-  },
-  function(err)
-  {
-      console.log(err||'connect-mongodb setup ok')
+app.use(
+  session({
+    name: "InsuranceApp",
+    secret: "blahblahblah...",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    store: MongoStore.create(
+      {
+        mongoUrl: `mongodb://127.0.0.1:27017/${process.env.CONNECTION_URL}`,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok")
+      }
+    ),
   })
-}))
-
+)
 
 //app.use(notFound)
 //app.use(errorHandler)
@@ -62,9 +65,6 @@ app.use(passport.setAuthenticatedUser)
 
 app.use(userRouter)
 
-
-
-app.listen(port,()=>{
-  console.log('Server is up on port '+ port)
+app.listen(port, () => {
+  console.log("Server is up on port " + port)
 })
-
